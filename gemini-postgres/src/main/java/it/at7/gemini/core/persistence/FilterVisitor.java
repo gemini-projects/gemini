8c0cc77d364f9ef5b0212315bdf2659aec81cca3
andreatarquini <tarquiniandreagm@gmail.com>
2019-02-18 23:26:07 +0100

    + [Gemini Filter] - refactor BasicType Filter Visitor

--- gemini-postgres/src/main/java/it/at7/gemini/core/persistence/FilterVisitor.java
+++ gemini-postgres/src/main/java/it/at7/gemini/core/persistence/FilterVisitor.java
@@ -5,18 +5,24 @@ import it.at7.gemini.exceptions.EntityFieldException;
 import it.at7.gemini.exceptions.GeminiRuntimeException;
 import it.at7.gemini.schema.Entity;
 import it.at7.gemini.schema.EntityField;
-import org.springframework.beans.factory.annotation.Autowired;
+import it.at7.gemini.schema.FieldType;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+import static it.at7.gemini.schema.FieldType.*;
 
 @Service
 public class FilterVisitor implements RSQLVisitor<String, Entity> {
+    private static final GeminiTypeFilterVisitor BASIC_TYPE_FILTER = new BasicTypeFilterVisitor();
 
-    private GeminiTypeFilterVisitor geminiTypeFileterVisitor;
 
-    @Autowired
-    public FilterVisitor(GeminiTypeFilterVisitor geminiTypeFileterVisitor) {
-        this.geminiTypeFileterVisitor = geminiTypeFileterVisitor;
-    }
+    Map<FieldType, GeminiTypeFilterVisitor> geminiTypeVisitors = Map.of(
+            TEXT, BASIC_TYPE_FILTER,
+            LONG, BASIC_TYPE_FILTER,
+            DOUBLE, BASIC_TYPE_FILTER,
+            NUMBER, BASIC_TYPE_FILTER
+    );
 
     @Override
     public String visit(AndNode node, Entity entity) {
@@ -33,7 +39,11 @@ public class FilterVisitor implements RSQLVisitor<String, Entity> {
         String selector = node.getSelector();
         try {
             EntityField field = entity.getField(selector);
-            return geminiTypeFileterVisitor.visit(field, node);
+            GeminiTypeFilterVisitor geminiTypeFilterVisitor = geminiTypeVisitors.get(field.getType());
+            if (geminiTypeFilterVisitor == null) {
+                throw new GeminiRuntimeException(String.format("Filter Not Implemented for type %s", field.getType()));
+            }
+            return geminiTypeFilterVisitor.visit(field, node);
         } catch (EntityFieldException e) {
             // TODO strict filter ? pass Context instead of Entity
             throw new GeminiRuntimeException("Filter Not Supported", e);
