eb45cb64c0105dff1f50cbe43de53d147b85163d
andreatarquini <tarquiniandreagm@gmail.com>
2019-04-30 19:56:45 +0200

    + [Boot] - Moved Boot to Postesql Module

--- gemini-postgresql/src/main/java/it/at7/gemini/boot/PostgresqlGeminiGUIMain.java
+++ gemini-postgresql/src/main/java/it/at7/gemini/boot/PostgresqlGeminiGUIMain.java
@@ -3,6 +3,8 @@ package it.at7.gemini.boot;
 import it.at7.gemini.api.Api;
 import it.at7.gemini.core.Gemini;
 import it.at7.gemini.gui.Gui;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,28 +12,31 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableAutoConfiguration
-public class AnnotationAppMain {
+public class PostgresqlGeminiGUIMain {
+    private static final Logger logger = LoggerFactory.getLogger(PostgresqlGeminiGUIMain.class);
+
     public static void main(String[] args) {
 
+        logger.info("***** STARTING GEMINI POSTRESQL MAIN *****");
+
+        logger.info("STARTING - GEMINI-ROOT APP CONTEXT ");
         ConfigurableApplicationContext root = new SpringApplicationBuilder()
-                .parent(Gemini.class, AnnotationAppMain.class).web(WebApplicationType.NONE)
+                .parent(Gemini.class, PostgresqlGeminiGUIMain.class).web(WebApplicationType.NONE)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
         root.setId("GEMINI-ROOT");
         Gemini gemini = root.getBean(Gemini.class);
         gemini.init();
+        logger.info("STARTED - GEMINI-ROOT APP CONTEXT");
 
-        /* ConfigurableApplicationContext api = new SpringApplicationBuilder()
-                .parent(root).sources(Api.class, AnnotationAppMain.class).web(WebApplicationType.SERVLET)
-                .bannerMode(Banner.Mode.OFF)
-                .run(args);
-        api.setId("GEMINI-API"); */
 
+        logger.info("STARTING - GEMINI-GUI APP CONTEXT ");
         ConfigurableApplicationContext gui = new SpringApplicationBuilder()
-                .parent(root).sources(Api.class, Gui.class, AnnotationAppMain.class).web(WebApplicationType.SERVLET)
+                .parent(root).sources(Api.class, Gui.class, PostgresqlGeminiGUIMain.class).web(WebApplicationType.SERVLET)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
         gui.setId("GEMINI-GUI");
+        logger.info("STARTED - GEMINI-GUI APP CONTEXT");
 
 
     }
